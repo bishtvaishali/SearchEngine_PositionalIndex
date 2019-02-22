0fa441de60e157dca50884808185bac3f15cd7c3
bishtvaishali <vaishali.bisht01@gmail.com>
2019-02-22 13:21:30 -0800

    index written to file, string is split in loading index

---
+++ src/Part1.java
@@ -0,0 +1,167 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.TreeMap;
+import org.lemurproject.kstem.KrovetzStemmer;
+
+
+public class Part1 {
+
+	/*
+	 * method is used to created Inverted Index. It takes a text file as a parameter.
+	 * It parses the file step by step(tokenization, normalization, stemming, stop words removal) 
+	 * and stores the terms, Doc Freq and Posting list in a dictionary(TreeMap). 
+	 */
+	public Map<String, ArrayList<Document>> createInvertedIndex (File file) throws IOException {
+
+		Map<String, ArrayList<Document>> dictionary = new TreeMap<>(); 	
+
+		//we create a uniqueDocuments for every document and check for duplicate words.
+
+		HashSet<String> stopWords = new HashSet<>();
+		stopWords.add("the");
+		stopWords.add("is");
+		stopWords.add("at");
+		stopWords.add("of");
+		stopWords.add("on");
+		stopWords.add("and");
+		stopWords.add("a");
+
+		KrovetzStemmer stemmer = new KrovetzStemmer();
+
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String[] splitStr;
+		String st;
+		int docId = 0;
+
+
+
+		while ((st = br.readLine()) != null) {
+			if(st.contains("</")) {
+				//do nothing
+			}else if(st.contains("<")) {
+				docId = Integer.parseInt(st.substring(5, st.length() - 1));
+			}else {		
+				splitStr = st.split("\\W+");
+
+				for(int i =0 ; i< splitStr.length; i++) {
+					splitStr[i] = splitStr[i].toLowerCase();
+				}
+				//System.out.println(Arrays.toString(splitStr));
+
+				int wordPosition = 0;
+				for (String word: splitStr) {
+					String stemmedWord = stemmer.stem(word);
+
+					//	System.out.println("\nWORD POSITION: [" + stemmedWord + "] POS:" + wordPosition );
+
+					if(!stopWords.contains(stemmedWord) && !stemmedWord.equals("")) {
+						//System.out.println("stemmedWord ->"+stemmedWord+"--docId==>"+docId);
+						if(!dictionary.containsKey(stemmedWord)) {
+							dictionary.put(stemmedWord, new ArrayList<Document>()); 
+						}
+
+
+						ArrayList<Document> docList = dictionary.get(stemmedWord);
+						if(docList.isEmpty()) {
+							docList.add(new Document(stemmedWord, docId));
+						}
+
+						boolean notFound = true;
+
+						for (Document doc : docList) {
+							if(doc.getDocId() == docId) {
+								doc.getPositionList().add(wordPosition);
+								//System.out.println("PL--:" + Arrays.toString(doc.getPositionList().toArray()));
+								notFound = false;
+							}	
+						}
+
+						if(notFound) {
+							docList.add(new Document(stemmedWord,docId));
+							for (Document doc : docList) {
+								if(doc.getDocId() == docId) {	
+									doc.getPositionList().add(wordPosition);
+									//System.out.println("PL--:" + Arrays.toString(doc.getPositionList().toArray()));
+									notFound = false;
+								}	
+							}
+
+						}
+					}
+					wordPosition += word.length() + 1;
+				}
+
+			}
+		}
+
+		br.close();
+
+		return dictionary;
+
+	}	
+
+
+	/*
+	 * class sorts the terms alphabetically and by docId.
+	 * sorting is done by implementing comparable.
+	 */
+
+	public class Document implements Comparable<Document>{
+
+		private String term;
+		private Integer docId;
+		private Integer termFreq;
+		private ArrayList<Integer> positionList;
+
+
+		public Document(String word, int docId) {
+			this.term = word;
+			this.docId = docId;
+			this.positionList = new ArrayList<Integer>();
+		}
+
+		public Integer getTermFreq() {
+			return termFreq;
+		}
+		public void setTermFreq(Integer termFreq) {
+			this.termFreq = termFreq;
+		}
+		public ArrayList<Integer> getPositionList() {
+			return positionList;
+		}
+
+		public String getTerm() {
+			return term;
+		}
+
+		public void setTerm(String term) {
+			this.term = term;
+		}
+		public Integer getDocId() {
+			return docId;
+		}
+		public void setDocId(Integer docId) {
+			this.docId = docId;
+		}
+
+		//for sorting terms alphabetically and by docId
+		@Override
+		public int compareTo(Document o) {
+			if(this.term.equals(o.term)) {
+				return this.docId - o.docId;
+			} else {
+				return this.term.compareTo(o.term);
+			}
+		}
+
+	}
+
+
+
+
+}
---
+++ src/Part2.java
@@ -0,0 +1,286 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+import javax.rmi.ssl.SslRMIClientSocketFactory;
+
+import org.lemurproject.kstem.KrovetzStemmer;
+
+
+
+
+public class Part2 {
+
+	final String PATH = "./invertedIdx.txt"; //location where invertedIndex file will be stored
+	/*
+	 * method takes 2 arguments,  posting lists of term1 and term2.
+	 * intersects the two posting lists and return the common docIds in an ArrayList of Integer type
+	 */
+
+	public ArrayList<Integer> intersect(ArrayList<Integer> p1, ArrayList<Integer> p2) {
+
+		ArrayList<Integer>  result = new ArrayList<>();
+
+		int i =0, j=0;
+		int p1Len = p1.size();
+		int p2Len = p2.size();
+
+		while(i < p1Len && j <  p2Len) {
+			if(p1.get(i) == p2.get(j)) {
+				int val = p1.get(i);
+				result.add(val);
+				i++;
+				j++;
+			}else if(p1.get(i) < p2.get(j)) {
+				i++;	
+			}else {
+				j++;
+			}
+		}
+		return result;
+	}
+
+
+	/*
+	 * takes two arguments - query and Dictionary
+	 * preProcess the query(tokenization, normalization...)
+	 * invokes method "intersect" for intersection
+	 * 
+	 */
+	
+	/*
+	public void queryEvaluation(String query, Map<String, ArrayList<Integer>> dictionary) throws IOException {
+
+
+		//preProcessing query
+		String[] splitStr = query.split(" ");
+
+		for(int i =0 ; i< splitStr.length; i++) {
+			splitStr[i] = splitStr[i].toLowerCase();
+		}
+		;
+		if(splitStr[1].equals("and")) {
+			KrovetzStemmer kroStemmer = new KrovetzStemmer();
+
+			String term1 = kroStemmer.stem(splitStr[0]);
+			String term2 = kroStemmer.stem(splitStr[2]);
+
+			//Intersection
+			System.out.println(query);
+			ArrayList<Integer> p1 = dictionary.get(term1);
+			ArrayList<Integer> p2 = dictionary.get(term2);	
+
+			Part2 part2 = new Part2();
+			ArrayList<Integer>  result = part2.intersect(p1, p2);
+
+			if(result.isEmpty()) {
+				System.out.println(query + ": No common Docid");
+			}else {
+				System.out.println("Result : " + result );
+				System.out.println("");
+
+				FileWriter fileWriter = new FileWriter("./result.txt", true);
+				BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+				bufferedWriter.write(query + " : "+ result + "\n");
+				bufferedWriter.close();
+			}
+		}else {
+
+			System.out.println("Cannot process query for " + splitStr[1] + "operator");
+		}
+
+	}
+
+*/
+
+
+
+	/*
+	 * takes 1 argument- Dictionary
+	 * create and write the Dictionary to a text file , "invertedIdx" in the current directory
+	 */
+
+	public void writeDictionaryToFile( Map<String, ArrayList<Part1.Document>> dictionary ) throws IOException {
+
+		//saving Dictionary to a file
+		FileWriter fstream; 
+		BufferedWriter out; 
+
+		fstream = new FileWriter(PATH); 
+		out = new BufferedWriter(fstream); 
+
+		for (Map.Entry<String, ArrayList<Part1.Document>> entry : dictionary.entrySet())
+		{
+			// System.out.println("key: " + entry.getKey() + "; value: " + entry.getValue());
+			ArrayList<Part1.Document> docArrList = entry.getValue();
+			ArrayList<Integer> intArrList = new ArrayList<>();
+
+			
+			System.out.print("[" + entry.getKey()+"]--> ");
+			
+			String documentList = "";
+
+			for(Part1.Document doc : docArrList) {
+				System.out.print( doc.getDocId() + Arrays.toString(doc.getPositionList().toArray()));
+				
+				documentList += "{" + doc.getDocId() + ","+ doc.getPositionList().size() + "}"+ Arrays.toString(doc.getPositionList().toArray());
+			}
+			
+			System.out.println();
+			
+			out.write(entry.getKey() + "," + docArrList.size() + documentList  + "\n"); 
+		} 
+		out.close(); 
+	}
+
+
+
+	/*
+	 * parse InvertedIndex from a file "invertedIdx." into the Dictionary.
+	 * returns map with term and posting list
+	 */
+	public Map<String, ArrayList<Part1.Document>> loadInvertedIdxFromFile() throws IOException {
+
+		Map<String, ArrayList<Part1.Document>> newDict = new TreeMap<>(); 
+
+		File file =  new File(PATH); 
+		BufferedReader br = new BufferedReader(new FileReader(file));		
+		String st;
+	
+		
+		while ((st = br.readLine()) != null) {
+			
+			System.out.println("LINE : " + st);
+			
+			int termIndex = st.indexOf(",");
+			//System.out.println("TERM INDEX:  " + ":" + termIndex);
+			
+			String term = st.substring(0, st.indexOf(","));
+			System.out.println("TERM: " + term);
+			
+			//DF
+			String subStr = st.substring(termIndex+1);
+			System.out.println("Substring 1 --->  " + subStr);
+			
+			//System.out.println("DF INDEX:  " + ":" + subStr.indexOf(","));
+			
+			boolean flag = true;
+			while(flag) {
+				
+				int curlyIdx1 = subStr.indexOf("{");
+				System.out.println("curlyStr --->  " + curlyIdx1);
+				
+				
+				int curlyIdx2 = subStr.indexOf("}");
+				
+				String postingStr = subStr.substring(curlyIdx1+1, curlyIdx2);
+				System.out.println("curlyStr --->  " + postingStr);
+				
+				String[] splitStr = postingStr.split(",");
+				
+				for(String str : splitStr) {
+					System.out.println("postingStr --->  " + str);
+				}
+				
+				
+				int sqIndex1 = subStr.indexOf("[");
+				int sqIndex2 = subStr.indexOf("]");
+				
+		
+				postingStr = subStr.substring(sqIndex1+1, sqIndex2);
+				System.out.println("sqStr --->  " + postingStr);
+				
+				splitStr = postingStr.split(",");
+				
+				for(String str : splitStr) {
+					System.out.println("postingStr --->  " + str);
+				}				
+				subStr = subStr.substring(sqIndex2+1);
+				System.out.println("LAST SUBSTRING --->  " + subStr);
+			
+				if(subStr.isEmpty()) {
+					flag= false;
+				}
+				
+			}
+			
+			System.out.println("\n------NEXT-------\n");
+			
+		}
+		
+		
+		
+		
+	
+		
+
+//		while ((st = br.readLine()) != null) {
+//
+//			st = st.replaceAll("\\s", "");
+//			st = st.replace("[", "");
+//			st = st.replace("]", "");
+//			String[] splitStr = st.split(",");
+//			
+//
+//			ArrayList<Integer> postingList = new ArrayList<Integer>();
+//
+//			for(int i=2; i< splitStr.length; i++) {
+//				postingList.add(Integer.parseInt(splitStr[i]));
+//			}
+//
+//			newDict.put(splitStr[0], postingList);
+//			
+//			
+//			
+//		}
+		br.close();
+
+		return newDict;
+	}
+
+
+
+	/*
+	 * invoke part1 to create Inverted Index and then save it to a file.
+	 * Load the inverted index from the file into a dictionary, newDict.
+	 * use the newDict to evaluate the queries by invoking part3 intersect function.
+	 */
+	public static void main(String[] args) throws Exception 
+	{ 
+		Part2 part2 = new Part2();
+
+		File docfile =  new File("./documents.txt");
+		String query1 = "asus AND google";
+		String query2 = "screen AND bad";
+		String query3 = "great AND tablet";
+		
+		
+		String test = "this AND Most";
+		
+
+		Part1 part1 = new Part1();
+
+		//part2 creates an inverted index and returns a dictionary
+		Map<String, ArrayList<Part1.Document>> dictionary = part1.createInvertedIndex(docfile);
+		part2.writeDictionaryToFile(dictionary);// a file is created with a name "invertedIdx.txt"
+		Map<String, ArrayList<Part1.Document>> newDict = part2.loadInvertedIdxFromFile(); //load the invertedIndx from file "invertedIdx.txt" into a Dictionary 
+
+		part2.queryEvaluation(test, newDict);
+//		part2.queryEvaluation(query1, newDict);
+//		part2.queryEvaluation(query2, newDict);
+//		part2.queryEvaluation(query3, newDict);
+
+	} 
+
+
+
+
+
+
+}
